b6ac3cee5385310a8e8475daf0567765185891e9
ronlamb <ron.lamb@gmail.com>
2021-12-18 17:44:20 -0500

    Some updates

--- MathLab/MathLab.cpp
+++ MathLab/MathLab.cpp
@@ -2,21 +2,16 @@
 //
 
 #include <iostream>
+#include <vector>
 #include "matrixbase.h"
+#include "mathvectorbase.h"
 
 int main()
 {
 	using namespace MathLib;
+	using namespace std;
 	MatrixBase<double> mat(3);
+	MathVectorBase<double> vect = { 1.0, 2.0, 3.0, 4.0 };
+
+	cout << "Vector: " << vect;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
