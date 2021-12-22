d38a61da26c6f881bb7cf4f06550096ecf3c02db
ronlamb <ron.lamb@gmail.com>
2021-12-22 16:32:03 -0500

    Tested in Open MP

--- MathLab/vector_functions.cpp
+++ MathLab/vector_functions.cpp
@@ -4,72 +4,40 @@
 #include "mathvectorbase.h"
 #include "thread_vector.h"
 #include "thread_vector_lambda.h"
+#include "thread_vector_omp.h"
 #include "instrumentation.h"
 #include "messages.h"
 
 using namespace MathLib;
 using namespace std;
 
-void simpleVectorDotProduct() {
-	MathVectorBase<double> v1 = { 1.0, 2.0, 3.0, 4.0 };
-	MathVectorBase<double> v2 = { 0.75, .0125, .175, .0675 };
-	double expected = 1.57;
-	const auto precision = std::cout.precision();
-	cout.precision(3);
-	cout << fixed 
-		<< line_separator << endl
-		<< v1 << endl << " . " << v2 << " = " 
-		<< std::setprecision(5) << v1.dotProduct(v2)
-		<< ", expected=" << expected << endl
-		<< line_separator << endl << endl;
-	cout.precision(precision);
-}
-
-void thdVectorDotProduct() {
-	ThreadVector<double, 10> v1 = { 1.0, 2.0, 3.0, 4.0, -2, -7, -0.11, 1, 0, 0,
-		                           0, -12, 1, 2, 3, -5, 3, 1, 2, 1, 7, .12, .93 };
-	ThreadVector<double, 10> v2 = { 0.75, .0125, .175, .0675, .5, -.1675, 1.111, .1, 120, 120,
-								   0, 1.22, .21, .72, 3, 2, -124, 1, 1, 1, -1, -1, 12
-								  };
+template <typename T> void vectorDotProduct() {
+	T v1 = { 1.0, 2.0, 3.0, 4.0, -2, -7, -0.11, 1, 0, 0,
+				  0, -12, 1, 2, 3, -5, 3, 1, 2, 1, 7, .12, .93 };
+	T v2 = { 0.75, .0125, .175, .0675, .5, -.1675, 1.111, .1, 120, 120,
+			 0, 1.22, .21, .72, 3, 2, -124, 1, 1, 1, -1, -1, 12 };
 	double expected = -376.22971;
 	const auto precision = std::cout.precision();
 	cout.precision(3);
 	cout << fixed
 		<< line_separator << endl
-		<< v1 << endl << " . " << v2 << " = " 
+		<< v1 << endl << " . " << v2 << " = "
 		<< std::setprecision(5) << v1.dotProduct(v2)
 		<< ", expected=" << expected << endl
 		<< line_separator << endl << endl;
 	cout.precision(precision);
 }
 
-void lamdaVectorDotProduct() {
-	ThreadVectorLambda<double, 10> v1 = { 1.0, 2.0, 3.0, 4.0, -2, -7, -0.11, 1, 0, 0,
-								   0, -12, 1, 2, 3, -5, 3, 1, 2, 1, 7, .12, .93  };
-	ThreadVectorLambda<double, 10> v2 = { 0.75, .0125, .175, .0675, .5, -.1675, 1.111, .1, 120, 120,
-								   0, 1.22, .21, .72, 3, 2, -124, 1, 1, 1, -1, -1, 12 };
-	double expected = -376.22971;
-
-	const auto precision = std::cout.precision();
-	cout.precision(3);
-	cout << fixed
-		<< line_separator << endl
-		<< v1 << endl << " . " << v2 << " = " 
-		<< std::setprecision(5) << v1.dotProduct(v2)
-		<< ", expected=" << expected << endl
-		<< line_separator << endl << endl;
-	cout.precision(precision);
-}
-void vectorTime(size_t size) {
-	MathVectorBase<double> v1(size);
-	MathVectorBase<double> v2(size);
+template <typename T> void vectorTime(size_t size, string message) {
+	T v1(size);
+	T v2(size);
 
 	for (size_t i = 0; i < size; ++i) {
 		v1[i] = i * .1372;
 		v2[i] = v1[i] - (i % 100) / 113.0;
 	}
 
-	string title = "vector " + to_string(size);
+	string title = message + " " + to_string(size);
 	for (size_t i = 0; i < 100; ++i) {
 		INSTR_NAME_START(tmp, title);
 		v1.dotProduct(v2);
@@ -77,46 +45,14 @@ void vectorTime(size_t size) {
 	}
 }
 
-void threadVectorTime(size_t size) {
-	ThreadVector<double> v1(size);
-	ThreadVector<double> v2(size);
-
-	for (size_t i = 0; i < size; ++i) {
-		v1[i] = i * .1372;
-		v2[i] = v1[i] - (i % 100) / 113.0;
-	}
-
-	string title = "thdvector " + to_string(size);
-	for (size_t i = 0; i < 100; ++i) {
-		INSTR_NAME_START(tmp, title);
-		v1.dotProduct(v2);
-		INSTR_NAME_END(tmp);
-	}
-
-}
-
-void lambdaVectorTime(size_t size) {
-	ThreadVectorLambda<double> v1(size);
-	ThreadVectorLambda<double> v2(size);
-
-	for (size_t i = 0; i < size; ++i) {
-		v1[i] = i * .1372;
-		v2[i] = v1[i] - (i % 100) / 113.0;
-	}
-
-	string title = "lamdavector " + to_string(size);
-	for (size_t i = 0; i < 100; ++i) {
-		INSTR_NAME_START(tmp, title);
-		v1.dotProduct(v2);
-		INSTR_NAME_END(tmp);
-	}
-
-}
 void vectorPerformance() {
-	simpleVectorDotProduct();
-	thdVectorDotProduct();
-	lamdaVectorDotProduct();
-	vectorTime(100000000);
-	threadVectorTime(100000000);
-	lambdaVectorTime(100000000);
+	vectorDotProduct<MathVectorBase<double>>();
+	vectorDotProduct<ThreadVector<double, 10>>();
+	vectorDotProduct<ThreadVectorLambda<double, 10>>();
+	vectorDotProduct<ThreadVectorOMP<double, 10>>();
+	size_t vectorSize = 100000000;
+	vectorTime<MathVectorBase<double>>(vectorSize, "vector");
+	vectorTime<ThreadVector<double, 10>>(vectorSize,"thdvector");
+	vectorTime<ThreadVectorLambda<double, 10>>(vectorSize, "lambdavector");
+	vectorTime<ThreadVectorOMP<double, 10>>(vectorSize, "ompvector");
 }
---
+++ MathLib/thread_vector_omp.h
@@ -0,0 +1,82 @@
+#pragma once
+
+/* OMP version of threaded class
+ *
+ * The template takes as parameters:
+ *	typename T				The type (double, long double)
+ *	size_t MIN_SLICE_SIZE	The minimum slice size, defaults to 10
+ *	size_t MAX_THREADS		The maximum number of threads
+ *
+ * It will calculate the number of threads needed to run with at least the minimum
+ * slice size.  The class will spawn up to the calculated number of threads - 1
+ * threads.
+ *
+ * If the size is less than MIN_SLICE_SIZE then it will only run in the current
+ * thread, and not spawn any more threads.
+ */
+#include "mathvectorbase.h"
+#include "mathexception.h"
+#include <thread>
+#include <omp.h>
+namespace MathLib {
+	template <typename T, size_t MIN_SLICE_SIZE = 10, size_t MAX_THREADS = 0>
+	class ThreadVectorOMP : public MathVectorBase<T>
+	{
+	protected:
+		using MathVectorBase<T>::size;
+		using MathVectorBase<T>::arr;
+		size_t num_threads;
+
+		void setNumThreads() {
+			num_threads = MAX_THREADS;
+			if (num_threads < 0) {
+				throw MathException(NUM_THREADS_LT_0);
+			}
+
+			if (num_threads > std::thread::hardware_concurrency() || num_threads == 0) {
+				num_threads = std::thread::hardware_concurrency();
+			}
+
+			size_t num_slices = size / MIN_SLICE_SIZE;
+			if (num_slices < 0) {
+				num_threads = 1;
+			}
+			else if (num_slices < num_threads) {
+				num_threads = num_slices;
+			}
+		}
+	public:
+		ThreadVectorOMP(size_t size) :MathVectorBase<T>(size) {
+			setNumThreads();
+		}
+
+		ThreadVectorOMP() :MathVectorBase<T>() {
+			setNumThreads();
+		}
+
+		ThreadVectorOMP(const std::initializer_list<T>& list) :MathVectorBase<T>(list) {
+			setNumThreads();
+		}
+		~ThreadVectorOMP() {}
+
+		T dotProduct(ThreadVectorOMP<T, MIN_SLICE_SIZE, MAX_THREADS>& v) {
+			using namespace std;
+			if (v.size != size) {
+				throw MathException(VECTOR_SIZES_MUST_BE_THE_SAME);
+			}
+
+			double sum = 0.0;
+			double* varr = v.arr;
+			double* arr1 = arr;
+			long i;
+
+			omp_set_num_threads(num_threads);
+			#pragma omp parallel for shared(arr1, varr) private(i) reduction(+:sum)
+			for (long i = 0; i < size; i++) {
+				sum += arr1[i] * varr[i];
+			}
+
+			return sum;
+		}
+	};
+};
